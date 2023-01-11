67e22ca70c38372f122b08c304ddc38030d44cb9
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-01-11 23:04:03 +0700

    feat : add migration for table threads

---
+++ migrations/1673452659724_create-table-threads.js
@@ -0,0 +1,32 @@
+/* eslint-disable camelcase */
+
+exports.up = (pgm) => {
+  pgm.createTable("threads", {
+    id: {
+      type: "VARCHAR(50)",
+      primaryKey: true,
+    },
+    body: {
+      type: "VARCHAR(100)",
+      notNull: true, 
+    },
+    title: {
+      type: "TEXT",
+      notNull: true,
+    },
+    owner: {
+      type: "VARCHAR(50)",
+      notNull: true,
+    },
+  })
+
+  pgm.addConstraint(
+    "threads",
+    "fk_threads.owner_users.id",
+    "FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE"
+  )
+}
+
+exports.down = (pgm) => {
+  pgm.dropTable("threads")
+}
