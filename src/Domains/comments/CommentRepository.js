9bf1f6da8c8622fc1e348f14f38afb984aefb2bb
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-01-22 15:05:05 +0700

    feat : add delete function in commentrepository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -4,6 +4,9 @@ class CommentRepository {
   async addComment(newComment) {
     throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
   }
+  async deleteComment(commentId) {
+    throw new Error("COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
+  }
 }
 
 module.exports = CommentRepository
