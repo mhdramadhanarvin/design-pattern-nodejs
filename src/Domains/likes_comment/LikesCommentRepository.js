350fa3213d232d6d9c2694b63e943413f252f7b4
Mhd. Ramadhan Arvin <mramadhan687@gmail.com>
2023-02-04 01:37:13 +0700

    feat : add abstract for like comment repository

---
+++ src/Domains/likes_comment/LikesCommentRepository.js
@@ -0,0 +1,12 @@
+/* eslint-disable no-unused-vars */
+
+class LikesCommentRepository {
+  async addUserComment(comment, owner) {
+    throw new Error("LIKES_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
+  } 
+  async removeUserComment(comment, owner) {
+    throw new Error("LIKES_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED")
+  } 
+}
+
+module.exports = LikesCommentRepository
---
+++ src/Domains/likes_comment/_test/LikesCommentRepository.test.js
@@ -0,0 +1,12 @@
+const LikesCommentRepository = require("../LikesCommentRepository")
+
+describe("LikesCommentRepository interface", () => {
+  it("should throw error when invoke abstract behavior", async () => {
+    // Arrange
+    const likesCommentRepository = new LikesCommentRepository()
+
+    // Action and Assert
+    await expect(likesCommentRepository.addUserComment({})).rejects.toThrowError("LIKES_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED") 
+    await expect(likesCommentRepository.removeUserComment({})).rejects.toThrowError("LIKES_COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED") 
+  })
+})
